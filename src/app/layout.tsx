344bb8b4c87e2bfdf41c6f8442d3497a8b3ca070
Arghya Mondal <arghyamondal.work@gmail.com>
2024-10-28 06:56:01 +0000

    clerk auth added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/theme/globals.css";
 import { ThemeProvider } from "@/theme/provider";
-import { Toaster } from "@/components/ui/toaster"
-
-
+import { Toaster } from "@/components/ui/toaster";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,18 +18,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-          <Toaster />
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider dynamic signInFallbackRedirectUrl={"/"} signUpFallbackRedirectUrl={"/"}>
+      <html lang="en" suppressHydrationWarning>
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+            <Toaster />
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
